f9f9f341f620142cec5a7bc22e79b76fc065048e
Robin Mukherjee <robin@otaking.co.uk>
2025-06-10 20:15:25 +0100

    add type for base64 functions

--- src/base64.tsx
+++ src/base64.tsx
@@ -1,9 +1,11 @@
-function base64ToBytes(base64) {
+type Base64String = string & { readonly __brand: 'Base64' }
+
+function base64ToBytes(base64: Base64String) {
   const binString = atob(base64)
-  return Uint8Array.from(binString, (m) => m.codePointAt(0))
+  return Uint8Array.from(binString, (m) => m.codePointAt(0)!)
 }
 
-function bytesToBase64(bytes) {
+function bytesToBase64(bytes: Uint8Array) {
   const binString = Array.from(bytes, (byte) =>
     String.fromCodePoint(byte)
   ).join('')
@@ -11,10 +13,10 @@ function bytesToBase64(bytes) {
 }
 
 export function stringToBase64(string: string) {
-  return bytesToBase64(new TextEncoder().encode(string))
+  return bytesToBase64(new TextEncoder().encode(string)) as Base64String
 }
 
-export function base64ToString(base64) {
+export function base64ToString(base64: Base64String) {
   return new TextDecoder().decode(base64ToBytes(base64))
 }
 
