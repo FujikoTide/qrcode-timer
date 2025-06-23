2fd7fbbf2ed9f9aa689d1f9f869a12053c5ad40a
Robin Mukherjee <robin@otaking.co.uk>
2025-06-23 23:46:40 +0100

    attempt fix for geocoding issue

--- src/components/molecules/LocationDisplayMap.tsx
+++ src/components/molecules/LocationDisplayMap.tsx
@@ -1,6 +1,7 @@
 // src/components/molecules/LocationDisplayMap.tsx
 import { Map, AdvancedMarker } from '@vis.gl/react-google-maps'
 import ContentColumn from './ContentColumn'
+import Typography from '../atoms/Typography'
 
 interface Coordinates {
   lat: number
@@ -14,6 +15,9 @@ interface LocationDisplayMapProps {
 export default function LocationDisplayMap({
   coordinates,
 }: LocationDisplayMapProps) {
+  if (!window.google || !window.google.maps) {
+    return <Typography>Loading Google Maps API...</Typography>
+  }
   return (
     <ContentColumn className="h-[300px] w-full lg:h-[400px]">
       <Map
